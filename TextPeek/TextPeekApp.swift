c9af6bd63a1c315b4726eb88ecf4efb08629231c
Ben Widdowson <github.27l3x@benwiddowson.com>
2026-03-17 17:13:16 +0700

    Initial Commit

---
+++ TextPeek/TextPeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextPeekApp.swift
+//  TextPeek
+//
+//  Created by Ben Widdowson on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TextPeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
